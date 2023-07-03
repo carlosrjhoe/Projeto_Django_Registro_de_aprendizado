6f9942f02c40c9c3734b45f4e6a6fdfe2c7038cc
Carlos Roberto <carlosrjhoe@gmail.com>
2023-07-02 22:44:56 -0300

    Django registro diário: Atualizando arquivos

--- venv/learning_logs/views.py
+++ venv/learning_logs/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
 
-
 # Create your views here.
 def index(request):
     """A página inicial de Learning Log"""
@@ -13,9 +12,9 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
-def topic(request, potic_id):
-    """Mostra um único assunto e todas as suas entradas."""
-    topic = Topic.objects.get(id=potic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
+# def topic(request, topic_id):
+#     """Mostra um único assunto e todas as suas entradas."""
+#     topic = Topic.objects.get(id=topic_id)
+#     entries = topic.entry_set.order_by('-date_added')
+#     context = {'topic': topic, 'entries': entries}
+#     return render(request, 'learning_logs/topic.html', context)
