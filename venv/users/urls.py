687620e5f01e18dfeb2349af293a2836a0429c76
Carlos Roberto <carlosrjhoe@gmail.com>
2023-07-10 12:58:54 -0300

    Django registro diário: Criando logout

--- venv/users/urls.py
+++ venv/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- venv/users/views.py
+++ venv/users/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def login(request):
+    """Faz login do usuário."""
     return render(request, 'users/login.html')
+
+def logout_view(request):
+    """Faz logout do usuário."""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
