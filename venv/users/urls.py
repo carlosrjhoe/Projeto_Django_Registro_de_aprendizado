d9ace40cecd5f134ce5f187be0f7a23f2c66ae97
Carlos Roberto <carlosrjhoe@gmail.com>
2023-07-14 18:43:10 -0300

    Django registro diário: Bug em url 'register do base.html'

--- venv/users/urls.py
+++ venv/users/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
+    # Página de login
     path('login/', views.login, name='login'),
+    # Página de logout
     path('logout/', views.logout_view, name='logout'),
+    # Página de registro
+    path('register/', views.register, name='register'),
 ]
